6acb3cf63386f631216ba3821d14d3a4b513bdf1
SeanLovesTea <sean.m.concannon@gmail.com>
2022-10-27 16:49:49 +0100

    added Menu page code and hooked up page load in index.js

--- modules/index.js
+++ modules/index.js
@@ -5,5 +5,27 @@ import createMenu from './menu.js';
 const content = document.querySelector('#content');
 content.appendChild(createHeader());
 content.appendChild(createHome());
-// content.appendChild(createMenu());
 
+addListeners();
+function addListeners(){
+    const navHome = document.querySelector('#homeBTN');
+    navHome.addEventListener('click',loadHome);
+    const navMenu = document.querySelector('#menuBTN');
+    navMenu.addEventListener('click',loadMenu);
+    // const navContact = document.querySelector('#contactBTN');
+    // navContact.addEventListener('click',);
+};
+
+function loadHome(){
+    content.replaceChildren();
+    content.appendChild(createHeader());
+    content.appendChild(createHome());
+    addListeners();
+};
+
+function loadMenu(){
+    content.replaceChildren();
+    content.appendChild(createHeader());
+    content.appendChild(createMenu());
+    addListeners();
+};
